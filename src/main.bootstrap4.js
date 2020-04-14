1d5da9345a811e18df03f8214b5da47014181ba6
Michal Remis <michal.remis@gmail.com>
2020-04-14 16:35:34 +0200

    Implemented field's specific wrapper_names to be passed to and used by CSV Javascript form builder for adding/removing of error messages. Before CSV used only form-wide wrapper but field may use custom wrapper specified:
        - by field's `wrapper` attribute
        - by field's type and `wrapper_mappings` attribute
        - by field's type and SimpleForm config `wrapper_mappings`

--- src/main.bootstrap4.js
+++ src/main.bootstrap4.js
@@ -3,13 +3,16 @@ import ClientSideValidations from '@client-side-validations/client-side-validati
 
 ClientSideValidations.formBuilders['SimpleForm::FormBuilder'] = {
   add: function (element, settings, message) {
-    this.wrapper(settings.wrapper).add.call(this, element, settings, message)
+    this.wrapper(this.wrapperName(element, settings)).add.call(this, element, settings, message)
   },
   remove: function (element, settings) {
-    this.wrapper(settings.wrapper).remove.call(this, element, settings)
+    this.wrapper(this.wrapperName(element, settings)).remove.call(this, element, settings)
   },
   wrapper: function (name) {
     return this.wrappers[name] || this.wrappers.default
+  },
+  wrapperName: function (element, settings) {
+    return element.data('clientSideValidationsWrapper') || settings.wrapper
   },
 
   wrappers: {
