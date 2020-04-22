ae7f72550dcd41824667648b5f6a89564f3180f5
Michal Remis <michal.remis@gmail.com>
2020-04-23 00:01:06 +0200

    ChckboxesWIP

--- src/radio_checkbox_validators.js
+++ src/radio_checkbox_validators.js
@@ -5,9 +5,11 @@ const originalPresenceValidator = ClientSideValidations.validators.local.presenc
 ClientSideValidations.validators.local.presence = (element, options) => {
   console.log(element);
   if (element.attr('type') === 'checkbox') {
-    console.log(element);
+    const formSettings = closest('form[data-client-side-validations=*]').data('clientSideValidations')
+    const wrapperTag = formSettings['html_settings']['wrapper_tag']
+    const wrapperClass = formSettings['html_settings']['wrapper_class']
 
-    if (element.closest('.form-group').find('input[type="checkbox"]:checked').length === 0) {
+    if (element.closest(`${wrapperTag}.${wrapperClass.replace(/ /g, '.')}`).find('input[type="checkbox"]:checked').length === 0) {
       return options.message
     }
   } else {
