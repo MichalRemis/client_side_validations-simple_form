afa0f53371152b9b6fafed1abc0c905ac6db5454
Michal Remis <michal.remis@gmail.com>
2020-04-27 21:42:25 +0200

    Add support for Date/Time inputs (bootstrap4)

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@
 
 source 'https://rubygems.org'
 
+gem 'client_side_validations', github: 'MichalRemis/client_side_validations', branch: 'SupportDateTimeSelects'
+
 gemspec
 
 gem 'client_side_validations', github: 'MichalRemis/client_side_validations', branch: 'attach-many-JS-validations'
--- src/main.bootstrap4.js
+++ src/main.bootstrap4.js
@@ -47,10 +47,11 @@ ClientSideValidations.formBuilders['SimpleForm::FormBuilder'] = {
     vertical_collection: {
       add (element, settings, message) {
         const wrapperElement = element.closest('.' + settings.wrapper_class.replace(/ /g, '.'))
+        const parentElement = element.parent()
         var errorElement = wrapperElement.find(settings.error_tag + '.invalid-feedback')
 
         if (!errorElement.length) {
-          errorElement = $('<' + settings.error_tag + '>', { class: 'invalid-feedback d-block', text: message })
+          errorElement = $('<' + settings.error_tag + '>', { class: 'invalid-feedback d-block', text: message })        
           element.closest('.form-check').parent().children('.form-check:last').after(errorElement)
           element.closest('.form-check').parent().children('.form-check:last').after(errorElement)
         }
@@ -67,7 +68,41 @@ ClientSideValidations.formBuilders['SimpleForm::FormBuilder'] = {
         errorElement.remove()
         wrapperElement.find('input:visible').removeClass('is-invalid')
       }
+    },
+    get horizontal_multi_select () {
+      return this.multi_select
+    },
+    get vertical_multi_select () {
+      return this.multi_select
+    },
+    multi_select: {
+      add (element, settings, message) {
+        const wrapperElement = element.closest(settings.wrapper_tag + '.' + settings.wrapper_class.replace(/ /g, '.'))
+        const parentElement = element.parent()
+        var errorElement = wrapperElement.find(settings.error_tag + '.invalid-feedback')
 
+        if (!errorElement.length) {
+          errorElement = $('<' + settings.error_tag + '>', { class: 'invalid-feedback d-block', text: message })
+          parentElement.after(errorElement)
+        }
+
+        wrapperElement.addClass(settings.wrapper_error_class)
+        element.addClass('is-invalid')
+        errorElement.text(message)
+      },
+      remove (element, settings) {
+        const wrapperElement = element.closest(settings.wrapper_tag + '.' + settings.wrapper_class.replace(/ /g, '.'))
+        const errorElement = wrapperElement.find(settings.error_tag + '.invalid-feedback')
+
+        const invalidSiblingExists = element.siblings('.is-invalid').length
+
+        if (!invalidSiblingExists) {
+          wrapperElement.removeClass(settings.wrapper_error_class)
+          errorElement.remove()
+        }
+
+        element.removeClass('is-invalid')
+      }
     }
   }
 }
